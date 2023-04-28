fda8cd312cb593f18c92c258ed0cc6f9993841c4
DevKalra <kalradev50@gmail.com>
2023-04-28 23:55:30 +0530

    commit

--- lib/Pages/workout_page.dart
+++ lib/Pages/workout_page.dart
@@ -1,8 +1,10 @@
+import 'package:fitness_app/modules/data.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class WorkoutPage extends StatefulWidget {
   final String workoutName;
-  const WorkoutPage({super.key,required this.workoutName});
+  const WorkoutPage({super.key, required this.workoutName});
 
   @override
   State<WorkoutPage> createState() => _WorkoutPageState();
@@ -11,10 +13,23 @@ class WorkoutPage extends StatefulWidget {
 class _WorkoutPageState extends State<WorkoutPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.workoutName),
-      ),
-    );
+    return Consumer<WorkoutData>(
+        builder: (context, value, child) => Scaffold(
+              appBar: AppBar(title: Text(widget.workoutName)),
+              body: ListView.builder(
+                itemCount: value.numberofExerciseInWorkout(widget.workoutName),
+                itemBuilder: (context, index) => ListTile(
+                  title: Text(
+                    value
+                        .getRelevantWorkout(widget.workoutName)
+                        .exercises[index]
+                        .name,
+                  ),
+                  subtitle: Row(
+                    children: [],
+                  ),
+                ),
+              ),
+            ));
   }
 }
